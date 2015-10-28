fe4382d94265eef396cabe6878e8a7deac6d1347
Eric <8BitRain@gmail.com>
2015-10-27 22:13:21 -0500

    Initial Commit

---
+++ StreetViewGyroScope/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StreetViewGyroScope
+//
+//  Created by Eric Smith on 10/27/15.
+//  Copyright © 2015 Eric Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
