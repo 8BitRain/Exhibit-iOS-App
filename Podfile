23c65e636b195fd3f64d3858d54430ce00d371ab
Eric <8BitRain@gmail.com>
2015-10-28 21:06:43 -0500

    Added Google Maps API
    Using Google Maps API to display default street view

---
+++ Podfile
@@ -0,0 +1,15 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '6.0'
+
+target 'StreetViewGyroScope' do
+pod 'GoogleMaps'
+end
+
+target 'StreetViewGyroScopeTests' do
+
+end
+
+target 'StreetViewGyroScopeUITests' do
+
+end
+
--- StreetViewGyroScope/ViewController.swift
+++ StreetViewGyroScope/ViewController.swift
@@ -7,19 +7,18 @@
 //
 
 import UIKit
+import GoogleMaps
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class ViewController: UIViewController, GMSMapViewDelegate {
+    
+    override func loadView() {
+        var panoView = GMSPanoramaView(frame: CGRectZero)
+        self.view = panoView
+        print("Test")
+        
+        panoView.moveNearCoordinate(CLLocationCoordinate2DMake(-33.732, 150.312))
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
+    
 }
 
