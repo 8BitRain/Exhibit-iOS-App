cc5fb6caf1e99a7fa6a289aa87e1719fc33fc7ff
croossin <croossin94@gmail.com>
2015-12-05 12:31:06 -0600

    Swapped www/

---
+++ ionicProj/myApp/www/js/app/controllers.js
@@ -0,0 +1,50 @@
+/**
+ * beginnings of a controller to login to system
+ * here for the purpose of showing how a service might
+ * be used in an application
+ */
+angular.module('app.controllers', [])
+    .controller('ListDetailCtrl', [
+        '$state', '$scope', '$stateParams', 'UserService',   // <-- controller dependencies
+        function ($state, $scope, $stateParams, UserService) {
+
+            $scope.index = $stateParams.itemId;
+
+        }])
+    .controller('ListCtrl', [
+        '$state', '$scope', 'UserService','AppService',   // <-- controller dependencies
+        function ($state, $scope, UserService, AppService) {
+
+            $scope.dataList = ["One", "Two", "Three", "Four"];
+
+
+
+            $scope.doLogoutAction = function () {
+                console.log("Logging out...");
+                UserService.logout().then(function () {
+
+                    // transition to next state
+                    $state.go('app-login');
+
+                }, function (_error) {
+                    alert("error logging in " + _error.debug);
+                })
+            };
+
+            //Called when user wants to refresh list
+            $scope.refreshList = function () {
+                AppService.queryExhibits();
+            };
+
+        }])
+    .controller('AccountCtrl', [
+        '$state', '$scope', 'UserService',   // <-- controller dependencies
+        function ($state, $scope, UserService) {
+
+            debugger;
+            UserService.currentUser().then(function (_user) {
+                $scope.user = _user;
+            });
+
+
+        }]);
---
+++ ionicProj/myApp/www/js/app/services.js
@@ -0,0 +1,27 @@
+angular.module('app.services', [])
+
+    .service('AppService', ['$q', 'ParseConfiguration',
+        function ($q, ParseConfiguration) {
+          return {
+
+            //Will query parse for Exhibits
+            queryExhibits: function () {
+              var UserList = Parse.Object.extend("_User");
+              var query = new Parse.Query(UserList);
+              query.find({
+                success: function(results) {
+                  alert("Successfully retrieved " + results.length + " users.");
+                  // Do something with the returned Parse.Object values
+                  for (var i = 0; i < results.length; i++) {
+                    var object = results[i];
+                    console.log(object.id + ' - ' + object.get('username'));
+                  }
+                },
+                error: function(error) {
+                  alert("Error: " + error.code + " " + error.message);
+                }
+              });
+            }
+
+          }
+        }]);
