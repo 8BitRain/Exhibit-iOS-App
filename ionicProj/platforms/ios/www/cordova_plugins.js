c454a5c8c9d4de8d3987fe5af6dfef86b1172cc5
croossin <croossin94@gmail.com>
2015-12-13 12:30:22 -0600

    working on orientation

--- ionicProj/platforms/ios/www/cordova_plugins.js
+++ ionicProj/platforms/ios/www/cordova_plugins.js
@@ -1,10 +1,8 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-<<<<<<< HEAD
 module.exports = [
     {
         "file": "plugins/cordova-plugin-screen-orientation/www/screenorientation.js",
         "id": "cordova-plugin-screen-orientation.screenorientation",
-        "pluginId": "cordova-plugin-screen-orientation",
         "clobbers": [
             "cordova.plugins.screenorientation"
         ]
@@ -12,17 +10,15 @@ module.exports = [
     {
         "file": "plugins/cordova-plugin-screen-orientation/www/screenorientation.ios.js",
         "id": "cordova-plugin-screen-orientation.screenorientation.ios",
-        "pluginId": "cordova-plugin-screen-orientation",
         "merges": [
             "cordova.plugins.screenorientation"
         ]
     }
 ];
-=======
-module.exports = [];
->>>>>>> 8581eaa4ca4e4edec30160fe3aed924225144b77
 module.exports.metadata = 
 // TOP OF METADATA
-{}
+{
+    "cordova-plugin-screen-orientation": "1.4.0"
+}
 // BOTTOM OF METADATA
 });
